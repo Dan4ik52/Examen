68377ebd3e4fd569ae55c19336a0b281a6670f95
Danik Makarenko <153374129+Dan4ik52@users.noreply.github.com>
2024-05-15 13:00:01 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine($"{x}+{y}={sum}");
+            */
+            /*
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("z= ");
+            int z = int.Parse(Console.ReadLine());
+            int sum = x + y +z;
+            Console.WriteLine($"{x}+{y}+{z}={sum}");
+            */
+            /*
+            Console.Write("x= ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            double y = double.Parse(Console.ReadLine());
+            double umn = x * y;
+            Console.WriteLine($"{x:f1}+{y:f1}={umn:f1}");
+            */
+            /*
+            Console.Write("x= ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            double y = double.Parse(Console.ReadLine());
+            double del = x / y;
+            Console.WriteLine($"{x:f3}/{y:f3}={del:f3}");
+            */
+            /*
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{x}+{y}={y}+{x}");    
+            */
+            /*
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            int sum = a+b;
+            Console.Write($"a+b={a}+{b}={sum}\t");
+            int min = a - b;
+            Console.Write($"a+b={a}-{b}={min}\t");
+            int umn = a * b;
+            Console.Write($"a+b={a}*{b}={umn}");
+            */
+            /*
+            Console.Write("Как тебя зовут? ");
+            string name = Console.ReadLine();
+            Console.Write("Сколько тебе лет? ");
+            int age = int.Parse(Console.ReadLine());
+            int year = 2009;
+            Console.Write($"{name}, ты родился в {year-age} году.");
+            */
+            /*
+            Console.Write("Номинал купюры = ");
+            int denomination = int.Parse(Console.ReadLine());
+            Console.Write("Количество купюр = ");
+            int quantity = int.Parse(Console.ReadLine());
+            Console.Write($"Сумма денег = {denomination*quantity:f2}р.");
+            */
+            /*
+            Console.Write("Введите длину стороны квадрата = ");
+            double length = double.Parse(Console.ReadLine());
+            double perimeter = 4 * length;
+            Console.Write($"Периметр квадрата = {perimeter}");
+            */
+            /*
+            Console.Write("Введите длину сторону a = ");
+            double lengtha = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину сторону a = ");
+            double lengthb = double.Parse(Console.ReadLine());
+            double perimeter = 2 * (lengtha+lengthb);
+            Console.Write($"Периметр прямоуг
+            Console.Write("Введите радиусльника = {perimeter}");
+            */
+            /* окружности = ");
+            double radius = double.Parse(Console.ReadLine());
+            double circumference = 2 * Math.PI * radius;
+            Console.Write($"Длинна окружности = {circumference:f2}");
+            */
+
+            Console.Write($"Введите обьем флешки в ГБ = ");
+            double volumeGB 
+            
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
